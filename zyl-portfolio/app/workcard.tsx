8e43328b3b5acffd26c562e8075fb0b5b646b0e4
Alyssa <41v1rj@gmail.com>
2023-07-26 05:48:28 -0700

    1

--- zyl-portfolio/app/workcard.tsx
+++ zyl-portfolio/app/workcard.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function WorkCard(){
+export default function WorkCard({iconadd, alttext, gifadd, children}: any){
+    const alttextIcon: string = alttext + 'pic';
+    const altextInter: string =  alttext + 'interaction';
     return (
         <div className="bg-white flex flex-col felx-grow shrink-0 rounded-md px-10 py-3 static border-2">
             <div className="flex flex-row">
-                <Image src='/Prelovedlogo.png'width={200} height={200} alt='Preloved logo pic'></Image>
-                <p className="text-slate-950 px-3">Preloved - a second-hand product e-commerce app based on community sections.</p>
+                <Image src={iconadd} width={200} height={200} alt={alttextIcon}></Image>
+                <p className="text-slate-950 px-3">{children}</p>
             </div>
             <div className=" pt-6">
-                <Image priority={true} src='/PrelovedInteraction.gif' width={960} height={720} alt='Preloved Interaction'></Image>
+                <Image priority={true} src={gifadd}width={960} height={720} alt={altextInter}></Image>
             </div>
         </div>
     )
